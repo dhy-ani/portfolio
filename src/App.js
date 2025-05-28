b05cabbc4a57eb2d2c67a5ef68ac5219a8f1e05d
dhy-ani <dhyanisoni77@gmail.com>
2025-05-28 18:37:32 -0400

    Resolved deploy issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-import About from './components/About'
+import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
 import './App.scss'
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
  
         </Route>
